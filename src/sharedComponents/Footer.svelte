3497e046a86442b9f3ab70a08e76fec138168d04
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-07-27 15:18:14 +0200

    footer WIP

---
+++ src/sharedComponents/Footer.svelte
@@ -0,0 +1,42 @@
+<script>
+
+</script>
+
+<footer>
+  <div class='footer-links'>
+    <div class='footer-left'>
+      <a href='/'>
+        <div class='logo-placeholder'></div>
+      </a>
+      <div class='social-links'>
+
+      </div>
+    </div>
+    <nav>
+      
+    </nav>
+  </div>
+</footer>
+
+<style>
+  footer {
+    background-color: var(--black);
+    color: var(--white);
+  }
+
+  .footer-links {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .footer-left {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .footer-socials {
+    display: flex;
+    flex-direction: row;
+  }
+</style>
