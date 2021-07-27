d7bf2df7337153b565076ea64bccbce6bc474031
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-07-27 15:05:26 +0200

    add routing for all pages

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,29 @@
 <script>
 	import page from 'page'
 	
+	import Nav from './sharedComponents/Nav.svelte'
+	import About from './About/About.svelte'
 	import Home from './Home/Home.svelte'
+	import GetInvolved from './GetInvolved/GetInvolved.svelte'
+	import MutualAid from './MutualAid/MutualAid.svelte'
+	import FAQ from './FAQ/FAQ.svelte'
 	import StrikeWithUs from './StrikeWithUs/StrikeWithUs.svelte'
 
 	let component = Home
 
 	page('', () => component = Home)
+	page('/about', () => component = About)
+	page('/get-involved', () => component = GetInvolved)
+	page('/mutual-aid', () => component = MutualAid)
+	page('/faq', () => component = FAQ)
 	page('/strike-with-us', () => component = StrikeWithUs)
-	page('/about', () => component = StrikeWithUs)
-	page('/get-involved', () => component = StrikeWithUs)
-	page('/faq', () => component = StrikeWithUs)
 	page('*', () => component = Home)
 	page.start()
 
 </script>
 
 <main>
+	<Nav />
 	<svelte:component this={component} />
 </main>
 
