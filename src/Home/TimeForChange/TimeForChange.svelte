18adea04095c0c0f8973fcd89abdd88f9f8ac709
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-07-28 19:03:16 +0200

    update its time to strike section with MVP text

--- src/Home/TimeForChange/TimeForChange.svelte
+++ src/Home/TimeForChange/TimeForChange.svelte
@@ -2,39 +2,48 @@
   const demands = [
     {
       title: 'Universal Healthcare',
-      snippet: 'Pharmaceutical companies are extorting patients for medications',
+      subheader: 'Pharmaceutical companies are extorting patients for medications',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
       title: 'Paid Parental Leave',
-      snippet: 'The United States is the only developed country that requires zero paid time off for maternity leave.',
+      subheader: 'The United States is the only developed country that requires zero paid time off for maternity leave.',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
       title: 'Coporate Accountability in Climate Change',
-      snippet: 'Only 100 corporations are responsible for 71% of all global carbon emissions.',
+      subheader: 'Only 100 corporations are responsible for 71% of all global carbon emissions.',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
       title: 'Corporate Tax Evasion',
-      snippet: 'Amazon paid $0 in taxes in 2017 and 2018.',
+      subheader: 'Amazon paid $0 in taxes in 2017 and 2018.',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
       title: 'Overworking & Burnout',
-      snippet: '78% of employees with 5 day weeks are less happy and more stressed than those with 4 day weeks.',
+      subheader: '78% of employees with 5 day weeks are less happy and more stressed than those with 4 day weeks.',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
       title: 'Minimum Wage & Income Inequality',
-      snippet: '40 million Americans live at\, or below the national poverty line. Current minimum wages are poverty wages.',
+      subheader: '40 million Americans live at\, or below the national poverty line. Current minimum wages are poverty wages.',
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     }
   ]
 
   const intersectionality = [
     {
-      title: "Race in the Working Class"
+      title: "Race in the Working Class",
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
-      title: "Disability in the Working Class"
+      title: "Disability in the Working Class",
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     },
     {
-      title: "Gender in the Working Class"
+      title: "Gender in the Working Class",
+      snippet: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat risus, nibh id egestas. Iaculis ultrices magna ac sed scelerisque blandit in ridiculus accumsan. Ac cursus diam posuere lectus.'
     }
   ]
 </script>
@@ -44,10 +53,11 @@
     The U.S. government is not serving its people. Now is the time for change.
   </h1>
   <div class='demands'>
-    {#each demands as { title, snippet }, i }
+    {#each demands as { title, subheader, snippet }, i }
       <div class='demand' class:darken={[1, 2, 5].includes(i)}>
         <h3>{title}</h3>
-        <h5>{snippet}</h5>
+        <h5>{subheader}</h5>
+        <h6>{snippet}</h6>
       </div>
     {/each}
   </div>
@@ -56,9 +66,10 @@
     The fight for the rights of the working class is an intersectional issue.
   </h2>
   <div class='intersectionalities'>
-    {#each intersectionality as { title }, i }
+    {#each intersectionality as { title, snippet }, i }
       <div class='intersectionality' class:darken={i ==1}>
         <h3>{title}</h3>
+        <h6>{snippet}</h6>
       </div>
     {/each}
   </div>
