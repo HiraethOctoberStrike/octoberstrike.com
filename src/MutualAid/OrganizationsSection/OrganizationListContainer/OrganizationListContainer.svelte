f9b6bb1377a408c98c1f4cdcf33e625c63ae7438
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-08-02 15:54:06 +0200

    style select and show more button

--- src/MutualAid/OrganizationsSection/OrganizationListContainer/OrganizationListContainer.svelte
+++ src/MutualAid/OrganizationsSection/OrganizationListContainer/OrganizationListContainer.svelte
@@ -12,7 +12,7 @@
   let selectedState
   let showMore = false
   let displayShowMoreButton = false
-  const defaultSelectionText = 'All Organizations'
+  const defaultSelectionText = 'Filter by State'
 
   const toggleShowMore = () => {
     showMore = !showMore
@@ -20,7 +20,7 @@
   }
 
   const filterByState = () => {
-    return mutualAidOrgs.filter( org => org.state === selectedState)
+    return mutualAidOrgs.filter( org => org.state == selectedState)
   }
 
   const createFilterOptions = () => {
@@ -35,15 +35,13 @@
     return uniqueStates
   }
 
-  const limitResults = (list) => {
-    return list.slice(0, 10)
-  }
+  const limitResults = (list) => list.slice(0, 10)
 
-  const filter = (mutualAidOrgs) => {
+  const filter = () => {
     displayOrgs = mutualAidOrgs
-    
+
     // if state selected, filter by state
-    if (selectedState !== defaultSelectionText) {
+    if (selectedState != defaultSelectionText) {
       displayOrgs = filterByState(mutualAidOrgs)
     }
 
@@ -62,28 +60,70 @@
 
       // remove blank lines
       mutualAidOrgs = data.filter( org => Object.values(org).join('') )
-
+      
       // set initial filter to default
       selectedState = defaultSelectionText
       
       filterOptions = createFilterOptions()
-      filter(mutualAidOrgs)
+      filter()
     })
   })
 
 </script>
 
 {#if mutualAidOrgs}
+
   <select bind:value={selectedState} on:change={filter}>
     {#each filterOptions as state}
-      <option value={state}>
-        {state}
-      </option>
+    <option value={state}>
+      {state}
+    </option>
     {/each}
   </select>
+
   <OrganizationList aidOrgs={displayOrgs} />
 
+
   {#if displayShowMoreButton }
-    <button on:click={toggleShowMore}>Show {showMore ? "Less" : "More"}</button>
+    <button on:click={toggleShowMore}>
+      <p>Show {showMore ? "Less" : "More"}</p>
+    </button>
   {/if}
 {/if}
+
+<style>
+  select {
+    background-color: var(--light-blue);
+    border: 1px solid var(--black);
+    color: var(--black);
+    padding: 12px 0 12px 20px;
+    margin-bottom: 30px;
+    font-size: 18px;
+  }
+
+  option {
+    background-color: var(--light-blue);
+    padding: 10px;
+  }
+
+  button {
+    width: 100%;
+    text-align: center;
+    border: none;
+    background-color: var(--lightest-grey);
+    padding: 10px;
+    transition: 0.1s ease-in;
+  }
+
+  button p {
+    color: var(--red);
+  }
+
+  button:hover {
+    background-color: var(--light-grey);
+  }
+
+  /* a:active {
+    background-color: var(--light-grey);
+  } */
+</style>
