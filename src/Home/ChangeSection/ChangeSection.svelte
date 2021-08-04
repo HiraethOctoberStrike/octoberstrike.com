b7ebda884688692b7712cffe0b320f58f8e0fbc6
Max Oppenheimer <maxoppenheimer1993@gmail.com>
2021-08-04 23:02:17 +0200

    update time for change section

--- src/Home/ChangeSection/ChangeSection.svelte
+++ src/Home/ChangeSection/ChangeSection.svelte
@@ -9,16 +9,30 @@ import { timeForChange } from '../../copy.js'
     The U.S. Government is not serving its people. Now is the time for change.
   </h2>
   <div class='demands'>
-    {#each timeForChange as { title, subtitles, snippet }, i }
-      <Card {title} red={[0, 4].includes(i)} navy={[3, 7].includes(i)}>
-        <div>
-          {#each subtitles as subtitle}
-            <h6>{@html subtitle}</h6>
-          {/each}
-          <p>{@html snippet}</p>
-        </div>
-      </Card>
-    {/each}
+    <div class='column'>
+      {#each timeForChange[0] as { title, subtitles, snippet }, i }
+        <Card {title} red={i % 2 === 0}>
+          <div>
+            {#each subtitles as subtitle}
+              <h6>{@html subtitle}</h6>
+            {/each}
+            <p>{@html snippet}</p>
+          </div>
+        </Card>
+      {/each}
+    </div>
+    <div class='column'>
+      {#each timeForChange[1] as { title, subtitles, snippet }, i }
+        <Card {title} navy={i % 2 !== 0}>
+          <div>
+            {#each subtitles as subtitle}
+              <h6>{@html subtitle}</h6>
+            {/each}
+            <p>{@html snippet}</p>
+          </div>
+        </Card>
+      {/each}
+    </div>
   </div>
 </section>
 
@@ -35,6 +49,13 @@ import { timeForChange } from '../../copy.js'
     margin: -16px;
   }
 
+  .column {
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 50%;
+    align-self: flex-start;
+  }
+
   h2 {
   	margin-bottom: 50px;
   }
@@ -42,4 +63,22 @@ import { timeForChange } from '../../copy.js'
   h6 {
     margin-bottom: 18px;
   }
+
+  @media (max-width: 1100px) {
+    .column {
+      flex: 1 1 100%;
+    }
+  }
+  
+  /* @media (max-width: 900px) {
+    .column {
+      flex: 1 1 50%;
+    }
+  }
+  @media (max-width: 800px) {
+    .column {
+      flex: 1 1 100%;
+    }
+  } */
+
 </style>
